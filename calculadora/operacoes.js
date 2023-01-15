2c348e7fb6050ef3b1175408d37470fcf4624dd8
Cibelle Maciel <cibellemaciel02@gmail.com>
2023-01-14 22:27:08 -0300

    add box resultado

--- calculadora/operacoes.js
+++ calculadora/operacoes.js
@@ -6,26 +6,28 @@ const button_div = document.getElementById("div")
 const n1 = document.getElementById("primeiro_numero")
 const n2 = document.getElementById("segundo_numero")
 
+const caixa_resultado = document.getElementById("resultado")
+
 button_som.onclick = () => {
     const a = n1.value
     const b = n2.value
-    alert(`Soma = ${Number(a) + Number(b)}`)
+    caixa_resultado.value = Number(a) + Number(b)
 }
 
 button_sub.onclick = () => {
     const a = n1.value
     const b = n2.value
-    alert(`Subtração = ${a - b}`)
+    caixa_resultado.value = a - b
 }
 
 button_mult.onclick = () =>{
     const a = n1.value
     const b = n2.value
-    alert(`Multiplicação = ${a * b}`)
+    caixa_resultado.value = a * b
 }
 
 button_div.onclick = () => {
     const a = n1.value
     const b = n2.value
-    alert(`Divisão = ${a / b}`)
+    caixa_resultado.value = a / b
 }
