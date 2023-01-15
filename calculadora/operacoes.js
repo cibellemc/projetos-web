e0e77473646397053611a3953c191f6cb3d3e22a
Cibelle Maciel <cibellemaciel02@gmail.com>
2023-01-15 12:17:26 -0300

    add: onclick display, clear, delete, result

--- calculadora/operacoes.js
+++ calculadora/operacoes.js
@@ -1,33 +1,49 @@
-const button_som = document.getElementById("som")
-const button_sub = document.getElementById("sub")
-const button_mult = document.getElementById("mult")
-const button_div = document.getElementById("div")
+// duas áreas de visualização, uma para os números correntes e outra para o resultado
+let cursor = document.querySelector(".numero_digitado")
+let resultado = document.querySelector(".resposta")
 
-const n1 = document.getElementById("primeiro_numero")
-const n2 = document.getElementById("segundo_numero")
+// inclui operadores e números
+const botoes = document.querySelectorAll("button")
 
-const caixa_resultado = document.getElementById("resultado")
+// "teclas especiais", com funções diferenciadas
+const limpar_tela = document.getElementById("limpar")
+const apagar = document.getElementById("apagar")
+const igual = document.getElementById("igual")
 
-button_som.onclick = () => {
-    const a = n1.value
-    const b = n2.value
-    caixa_resultado.value = Number(a) + Number(b)
+// ao clicar numa tecla, ela será acrescida ao número digitado e sera exibido em "tempo real" o resultado
+botoes.forEach(element => {
+    element.onclick = () => {
+        //inner.html retorna o conteúdo
+        cursor.innerHTML += element.value
+        resultado.innerHTML = eval(cursor.innerHTML)
+    }
+});
+
+//quando limpar, precisa limpa a operação corrente e zera o resultado
+limpar_tela.onclick = () => { 
+    cursor.innerHTML = " "
+    resultado.innerHTML = "0"
+
+// reseta as propriedades css
+    cursor.className = "numero_digitado"
+    resultado.className = "resposta"
 }
 
-button_sub.onclick = () => {
-    const a = n1.value
-    const b = n2.value
-    caixa_resultado.value = a - b
+apagar.onclick = () => {
+    // garantir que somente ao clicar em igual as propriedades serão trocadas
+    cursor.className = "numero_digitado"
+    resultado.className = "resposta"
+
+    if (cursor.textContent) { 
+        // se tiver número digitado armazena esse valor
+        let numero_corrente = document.querySelector(".numero_digitado").innerHTML
+        // o cursor vai receber o conteúdo decrescido do último termo - funciona como um pop()
+        cursor.innerHTML = numero_corrente.substring(0, numero_corrente.length - 1)
+    }
 }
 
-button_mult.onclick = () =>{
-    const a = n1.value
-    const b = n2.value
-    caixa_resultado.value = a * b
-}
-
-button_div.onclick = () => {
-    const a = n1.value
-    const b = n2.value
-    caixa_resultado.value = a / b
+igual.onclick = () => {
+    // "inverte" as propriedades css, a resposta fica maior
+    cursor.className = "resposta"
+    resultado.className = "numero_digitado"
 }
