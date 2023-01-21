cd7e78c855c062bbb97df0ba63f6ffdaa44ab4b1
Cibelle Maciel <cibellemaciel02@gmail.com>
2023-01-21 16:00:00 -0300

    fix: parar e pausar, contagem de minutos e horas

--- cronometro/script.js
+++ cronometro/script.js
@@ -1,32 +1,54 @@
 let sec = 0
 let min = 0
+let hr = 0
+let tempo 
 
 function pause() {
-    console.log("pausei");
+    tempo = clearInterval(tempo)
 }
 
 function stop(){
-    console.log("parei");
-
+    tempo = clearInterval(tempo)
+    sec = "00"
+    min = "00"
+    hr = "00"
+    document.getElementById("horas").innerHTML = sec
+    document.getElementById("minutos").innerHTML = sec
+    document.getElementById("segundos").innerHTML = sec
 }
 
 function start() {
-    setInterval(count_sec, 1000)
+    tempo = setInterval(count_sec, 1000)
 }
 
 function count_sec (){
     sec++
-    if(sec == 60) {
-        sec = 0 + "0"
-        count_min()
+
+    // verificar se é diferente de 0 pois fica 000
+    if (sec != 0 && sec <= 9) {
+        document.getElementById("segundos").innerHTML = "0" + sec
+    } else {
+        if(sec == 60) {
+            sec = "00"
+            count_min()
+        }
+        document.getElementById("segundos").innerHTML = sec
     }
-    document.getElementById("segundos").innerHTML = "0" + sec
 }
 
 function count_min() {
     min++
-    if(min == 60) {
+
+    if (min <= 59) {
+        document.getElementById("minutos").innerHTML = "0" + min
+    } else {
         min = 0 + "0"
+        count_hr()
+        document.getElementById("minutos").innerHTML = min
     }
-    document.getElementById("minutos").innerHTML = "0" + min
+}
+
+function count_hr() {
+    hr++
+    document.getElementById("horas").innerHTML = "0" + hr
 }
