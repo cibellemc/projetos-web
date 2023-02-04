9d8e2f1cb49b397e3c6d767f1a3abbf2d3f948dd
Cibelle Maciel <cibellemaciel02@gmail.com>
2023-02-04 15:45:50 -0300

    add: funções onclick - X e O

--- jogo-da-velha/script.js
+++ jogo-da-velha/script.js
@@ -6,16 +6,55 @@ allBox = document.querySelectorAll(".game button")
 player = document.querySelector(".players")
 
 window.onload = () => {
+    for (let i = 0; i < allBox.length; i++) {
+        allBox[i].setAttribute("onclick", "clickedBox(this)")   
+    }
+
     selectXBtn.onclick = () => {
         selectBox.classList.add("hide")
         player_box.classList.add("show")
     }
+
     selectOBtn.onclick = () => {
         selectBox.classList.add("hide")
         player_box.classList.add("show")
-        player.setAttribute("class", "players active player")
+        player.setAttribute("class", "player active players")
     }
 }
 
-let playerXIcon = "fa fa-times"
-let playerOIcon = "fa fa-circle"
+function clickedBox(element){
+    // quem começar fica ativo, bota o contrário
+    if(player.classList.contains("player")){
+        //tem que ter ``
+        element.innerHTML = `<i class="fa-regular fa-circle"></i>`
+        player.classList.add("active")
+    } else {
+        element.innerHTML = `<i class="fa-solid fa-xmark"></i>`
+        player.classList.add("active")
+    }
+    element.style.pointerEvents = "none"
+    bot()
+}   
+
+function bot(){
+    let array = []
+    for (let i = 0; i < allBox.length; i++) {
+        if(allBox[i].childElementCount == 0) {
+            array.push(i)
+        }        
+    }
+
+    let randomBox = array[Math.floor(Math.random() * array.length)]
+
+    if(array.length > 0){ //se já tiver começado, coloca na posição aleatória e o símbolo contrario
+        if(player.classList.contains("player")){
+            //tem que ter ``
+            allBox[randomBox].innerHTML = `<i class="fa-solid fa-xmark"></i>`
+            player.classList.add("active")
+        } else {
+            allBox[randomBox].innerHTML = `<i class="fa-regular fa-circle"></i>`
+            player.classList.add("active")
+        }
+    }
+}
+
