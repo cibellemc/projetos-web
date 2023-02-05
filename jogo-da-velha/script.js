b5dfd3a5c8e519698fdf1770fb870aaa07642029
Cibelle Maciel <cibellemaciel02@gmail.com>
2023-02-04 22:40:31 -0300

    fix: versão quase final

--- jogo-da-velha/script.js
+++ jogo-da-velha/script.js
@@ -3,7 +3,7 @@ selectXBtn = selectBox.querySelector(".playerX")
 selectOBtn = selectBox.querySelector(".playerO")
 player_box = document.querySelector(".player-box")
 allBox = document.querySelectorAll(".game button")
-player = document.querySelector(".players")
+player = document.querySelector(".player")
 
 window.onload = () => {
     for (let i = 0; i < allBox.length; i++) {
@@ -18,13 +18,13 @@ window.onload = () => {
     selectOBtn.onclick = () => {
         selectBox.classList.add("hide")
         player_box.classList.add("show")
-        player.setAttribute("class", "player active players")
+        player.setAttribute("class", "player active circle")
     }
 }
 
 function clickedBox(element){
     // quem começar fica ativo, bota o contrário
-    if(player.classList.contains("player")){
+    if(player.classList.contains("circle")){
         //tem que ter ``
         element.innerHTML = `<i class="fa-regular fa-circle"></i>`
         player.classList.add("active")
@@ -47,14 +47,13 @@ function bot(){
     let randomBox = array[Math.floor(Math.random() * array.length)]
 
     if(array.length > 0){ //se já tiver começado, coloca na posição aleatória e o símbolo contrario
-        if(player.classList.contains("player")){
+        if(player.classList.contains("circle")){
             //tem que ter ``
             allBox[randomBox].innerHTML = `<i class="fa-solid fa-xmark"></i>`
-            player.classList.add("active")
+            player.classList.remove("active")
         } else {
             allBox[randomBox].innerHTML = `<i class="fa-regular fa-circle"></i>`
-            player.classList.add("active")
+            player.classList.remove("active")
         }
     }
 }
-
