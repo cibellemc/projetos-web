64081edbf55b8f934f18ecf3979a2249e4f5ca5f
Cibelle Maciel <cibellemaciel02@gmail.com>
2023-03-26 17:10:53 -0300

    add: responsive navbar

---
+++ empresa/script.js
@@ -0,0 +1,6 @@
+const menuHamburguer = document.querySelector(".menu-hamburguer")
+const navLinks = document.querySelector(".nav-links")
+
+menuHamburguer.addEventListener('click', () => {
+    navLinks.classList.toggle('mobile-menu')
+})
