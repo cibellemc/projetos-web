418f629de2bcb00b7204c46edb0c3090504d0cbb
Cibelle Maciel <cibellemaciel02@gmail.com>
2023-04-08 16:38:47 -0300

    add: slider

--- empresa/script.js
+++ empresa/script.js
@@ -1,6 +1,36 @@
-const menuHamburguer = document.querySelector(".menu-hamburguer")
-const navLinks = document.querySelector(".nav-links")
+const slider = document.querySelectorAll('.slider');
+const btnPrev = document.getElementById('prev-button');
+const btnNext = document.getElementById('next-button');
 
-menuHamburguer.addEventListener('click', () => {
-    navLinks.classList.toggle('mobile-menu')
-})
+let currentSlide = 0;
+
+function hideSlider() {
+  slider.forEach(item => item.classList.remove('on'))
+}
+
+function showSlider() {
+  slider[currentSlide].classList.add('on')
+}
+
+function nextSlider() {
+  hideSlider()
+  if(currentSlide === slider.length -1) {
+    currentSlide = 0
+  } else {
+    currentSlide++
+  }
+  showSlider()
+}
+
+function prevSlider() {
+  hideSlider()
+  if(currentSlide === 0) {
+    currentSlide = slider.length -1
+  } else {
+    currentSlide--
+  }
+  showSlider()
+}
+
+btnNext.addEventListener('click', nextSlider)
+btnPrev.addEventListener('click', prevSlider)
