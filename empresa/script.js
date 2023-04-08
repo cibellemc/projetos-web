9fadc8a3579fe39ff7eafc9b7b65c4e5537e4977
Cibelle Maciel <cibellemaciel02@gmail.com>
2023-04-08 17:40:44 -0300

    add: tornar responsivo

--- empresa/script.js
+++ empresa/script.js
@@ -1,36 +1,10 @@
-const slider = document.querySelectorAll('.slider');
-const btnPrev = document.getElementById('prev-button');
-const btnNext = document.getElementById('next-button');
+let menu = document.querySelector("#menu-icon")
+let navbar = document.querySelector(".navbar")
 
-let currentSlide = 0;
+menu.addEventListener("click", function(){
+    navbar.classList.toggle("active")
+})
 
-function hideSlider() {
-  slider.forEach(item => item.classList.remove('on'))
+window.onscroll = () => {
+    navbar.classList.remove("active")
 }
-
-function showSlider() {
-  slider[currentSlide].classList.add('on')
-}
-
-function nextSlider() {
-  hideSlider()
-  if(currentSlide === slider.length -1) {
-    currentSlide = 0
-  } else {
-    currentSlide++
-  }
-  showSlider()
-}
-
-function prevSlider() {
-  hideSlider()
-  if(currentSlide === 0) {
-    currentSlide = slider.length -1
-  } else {
-    currentSlide--
-  }
-  showSlider()
-}
-
-btnNext.addEventListener('click', nextSlider)
-btnPrev.addEventListener('click', prevSlider)
