f939615ef4905cfb50e0ac495b306943351bed4b
Cibelle Maciel <108502128+cibellemc@users.noreply.github.com>
2023-11-06 08:42:52 -0300

    Update script.js

--- fancyapp-video/script.js
+++ fancyapp-video/script.js
@@ -1,11 +1,21 @@
 $(document).ready(function () {
-    var numberOfLinks = $("a.miniatura-camera").length;
+    const imgBox = document.querySelector('.img-box');
+    const containerImgs = imgBox.querySelectorAll('.container-img');
+    const numContainers = containerImgs.length;
 
-    // Calcula o width com base no número de elementos
-    var colWidth = 100 / numberOfLinks*2 + "%";
+    // número desejado de colunas e linhas
+    const numCols = 3;
+    const numRows = Math.ceil(numContainers / numCols);
 
-    // Define o width das col-4
-    $(".col-4").css("width", colWidth);
+    // porcentagens com base no número de colunas e linhas
+    const widthPercentage = 100 / numCols;
+    const heightPercentage = 100 / numRows;
+
+    // porcentagens de altura e largura aos elementos .container-img
+    containerImgs.forEach(function (container) {
+        container.style.width = `calc(${widthPercentage}% - 2px)`; // Subtrai 2px para acomodar as bordas
+        container.style.height = `calc(${heightPercentage}% - 2px)`; // Subtrai 2px para acomodar as bordas
+    });
 
     $(".miniatura-camera").fancybox({
         buttons: [
