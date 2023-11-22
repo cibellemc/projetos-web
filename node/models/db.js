be4aa3296d383fd7f61433d7db52109246b85751
Cibelle Maciel <108502128+cibellemc@users.noreply.github.com>
2023-11-22 10:19:43 -0300

    Delete node directory

--- node/models/db.js
+++
@@ -1,8 +0,0 @@
-import Sequelize from 'sequelize';
-
-const sequelize = new Sequelize('curso-node', 'postgres', '01111971', {
-    dialect: 'postgres',
-    host: 'localhost'
-  })
-
-export { Sequelize, sequelize };
