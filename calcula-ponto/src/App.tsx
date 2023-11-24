7cec5db04e4c42c2e2462cb8b5ab5f61082968c2
Cibelle Maciel <108502128+cibellemc@users.noreply.github.com>
2023-11-24 08:18:23 -0300

    add: cálculo de tempo restante de intervalo

--- calcula-ponto/src/App.tsx
+++ calcula-ponto/src/App.tsx
@@ -1,13 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import moment from 'moment';
 
 function App() {
   const [horaInicio, setHoraInicio] = useState(moment().format('HH:mm'))
-  const [intervalo, setIntervalo] = useState("01:15:00")
+  const [intervalo, setIntervalo] = useState("01:15")
+  const [tempoRestante, setTempoRestante] = useState("1:15")
 
   const horaRetorno = moment(horaInicio, 'HH:mm').add(moment(intervalo, 'HH:mm').hours(), 'hours').add(moment(intervalo, 'HH:mm').minutes(), 'minutes').format('HH:mm');
-  const tempoRestante = moment(horaRetorno, 'HH:mm').subtract(moment().format('HH:mm')).subtract(moment(horaInicio).format('HH:mm')).format('HH:mm');
+  const claculoTempoRestante = moment(horaRetorno, 'HH:mm').subtract(moment().format('HH:mm')).subtract(moment(horaInicio).format('HH:mm')).format('HH:mm');
+
+  useEffect(() => {
+    const agora = moment().format("HH:mm");
+
+    // se o input do usuário é de hora no futuro
+    if (moment(horaInicio, 'HH:mm').isAfter(moment(agora, 'HH:mm'))) {
+      setTempoRestante("01:15")
+    // se a hora atual é menor que a hora de retorno
+    } else if (moment(agora, 'HH:mm').isBefore(moment(horaRetorno, 'HH:mm'))) {
+      setTempoRestante(claculoTempoRestante)
+    } else {
+      setTempoRestante("00:00")
+    }
+  })
 
   function handleOnChange(event: { target: { value: any; }; }) {
     const inputValue = event.target.value;
@@ -36,7 +51,7 @@ function App() {
         <input type="time" onChange={handleChangeIntervalo} value={intervalo} id="" />
       </div>
       <h2>Volta às {horaRetorno}</h2>
-      {/* <h2>Tempo restante: {tempoRestante}</h2> */}
+      <h2>Tempo restante: {tempoRestante}</h2>
 
     </div>
   )
